51b1c1c4e84b2a0fc20f6336b4ec5c12042479f9
asuka-laptopus <>
2023-09-12 11:19:28 +0200

    created new starting scripts

--- turing-script.sh
+++ turing-script.sh
@@ -1,3 +1,3 @@
 #! /bin/zsh
 cd /Users/asuka/Documents/projects/LLaMA2/llama.cpp
-./main -t 1 -ngl 32 -m models/llama-2-7b.q4_K_M.gguf --color -c 2048 --temp 0.7 --repeat_penalty 1.1 -n 256 -i -r "User:" --in-prefix " " -f prompts/chat-turing.txt
+./main -t 1 -ngl 32 -m models/llama-2-7b.q4_K_M.gguf --color -c 2048 --temp 0.7 --repeat_penalty 1.1 -n 256 -i -r "User:" --in-prefix " " -f ../llama2-my_files/prompts/chat-turing.txt
